d5abee688976636789f199087c5825d0cc66352c
sylvainpeter <lexriders@gmail.com>
2022-10-29 10:03:55 +0200

    Met à jour les commentaires

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -13,15 +13,17 @@ const User = require('../models/userModel');
 // SIGNUP (OK)
 
 exports.signup = (req, res, next) => {
-  bcrypt.hash(req.body.password, 10) // hashe le mot de passe récupéré dans le body (avec 10 cycles de salage)
-    .then(hash => { // on récupère le hash
-      const user = new User({ // on créé un nouvel User
+  // hashe le password envoyé dans le body (avec 10 cycles de salage)
+  bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+      // on créé un nouvel utilisateur
+      const user = new User({ 
         pseudo: req.body.pseudo,
         email: req.body.email,
-        password: hash, // le mot de passe crypté
+        password: hash, // le password qui vient d'être hashé
         isAdmin: false,
       });
-      user.save() // on sauvegarde le nouvel User dans la BDD
+      user.save() // on sauvegarde le nouvel utilisateur dans la BDD
         .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
         .catch(error => res.status(400).json({ error }));
     })
@@ -31,22 +33,26 @@ exports.signup = (req, res, next) => {
 // LOGIN (OK)
 
 exports.login = (req, res, next) => {
+  // on cherche dans la BDD l'utilisateur qui correspond à l'email envoyé
   User.findOne({ email: req.body.email })
     .then(user => {
+      // si l'utilisateur n'existe pas
       if (!user) {
         return res.status(401).json({ error: 'Utilisateur non trouvé !' });
       }
-      bcrypt.compare(req.body.password, user.password) // Compare le mot de passe entré avec le hash stocké dans la BDD
+       // si l'utilisateur existe, on compare le password envoyé avec le hash stocké
+      bcrypt.compare(req.body.password, user.password)
         .then(valid => {
           if (!valid) {
             return res.status(401).json({ error: 'Mot de passe incorrect !' });
           }
+          // si les deux correspondent, on crée un nouveau token
           res.status(200).json({
             userId: user._id,
-            token: jwt.sign( // Chiffre un nouveau token
+            token: jwt.sign(
               { userId: user._id, isAdmin: user.isAdmin },
               process.env.RANDOM_SECRET_TOKEN, // chaîne secrète
-              { expiresIn: '24h' } // définit la durée de validité du token à 24 heures
+              { expiresIn: '24h' } // le token est valide pendant 24 heures
             ),
             isAdmin: user.isAdmin,
             pseudo: user.pseudo
